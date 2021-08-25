035fa8e5c6457315d129671ee3e26a7640d67617
Adam <adam@example.com>
2021-08-24 21:58:18 -0600

    check box clicable

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,17 @@
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onUpdateTodo }) => {
   return (
     <ul className="list-group">
       {todos.map((todo) => (
-        <li class="list-group-item d-flex justify-content-between align-items-center">
+        <li
+          key={todo.id}
+          class="list-group-item d-flex justify-content-between align-items-center"
+        >
           {todo.title}
-          <input type="checkbox" checked={todo.completed} />
+          <input
+            type="checkbox"
+            checked={todo.completed}
+            onChange={() => onUpdateTodo(todo)}
+          />
         </li>
       ))}
     </ul>
